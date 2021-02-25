fade1692e7cd618c6880f7ee62ceec8c4e99ea66
Tohzen <melvin.l.mobley@gmail.com>
2021-02-25 16:37:24 -0500

    finished

--- color-cleaver.js
+++ color-cleaver.js
@@ -3,5 +3,14 @@ const colorDeconstructor = require("./color-deconstructor.js");
 const isValidPrimary = require("./is-valid-primary.js");
 const isValidSecondary = require("./is-valid-secondary.js");
 const getInput = require("./get-input.js");
-
+const str1 = getInput(1);
+const str2 = getInput(2);
 // Your code here!
+
+
+if(str1 !== undefined && str2 !== undefined && isValidPrimary(str1) && isValidPrimary(str2)){
+    console.log("If we put them together it makes " + colorCombinator(str1,str2) + "!")
+}
+if(getInput(1) !== undefined && getInput(2) == undefined ){
+    console.log("If we split that color apart we get " + colorDeconstructor(str1) + "!") 
+}
